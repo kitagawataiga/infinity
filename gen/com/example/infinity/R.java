10422af8e9f3c84c68d38fda9c1a633d23c816e3
hatada <student@PC63409S.fgakusei.local>
2013-10-04 14:56:17 +0900

    t

--- gen/com/example/infinity/R.java
+++ gen/com/example/infinity/R.java
@@ -31,7 +31,9 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int menu_select=0x7f030001;
-        public static final int test=0x7f030002;
+        public static final int t=0x7f030002;
+        public static final int test=0x7f030003;
+        public static final int test1=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
